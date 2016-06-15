7960d5dd3898532131ee6c2880f6a3a2a60e4a6a
Tbtimber <delpierre.matthieu@gmail.com>
2016-06-15 10:17:39 +0200

    Drawer setu and working

--- app/src/main/java/com/timber/mdelpierre/budgeter/MainActivity.java
+++ app/src/main/java/com/timber/mdelpierre/budgeter/MainActivity.java
@@ -1,14 +1,67 @@
 package com.timber.mdelpierre.budgeter;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @Bind(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+
+    @Bind(R.id.left_drawer)
+    ListView mDrawerList;
+
+    //Private field
+    private String[] mPlanetTiles = {"Earth","Neptune","URANUS"};
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Test
+
+        ButterKnife.bind(this);
+
+        mDrawerTitle = mTitle = getTitle();
+        //getActionBar().set
+
+        setSupportActionBar(mToolbar);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_tv, mPlanetTiles));
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,  mToolbar, R.string.drawer_open, R.string.drawer_closed) {
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+        };
+        mDrawerToggle.syncState();
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+
+
     }
 }
